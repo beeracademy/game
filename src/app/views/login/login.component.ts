50b79d1a0cea0a1c8ded48941ca4471fd6d338c2
Jonas Tranberg <jonas_ts@msn.com>
2019-10-03 20:43:12 +0200

    Change tab color on mobile based on what view you are in

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -10,6 +10,7 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.querySelector('meta[name=theme-color]').setAttribute('content', '#fff');
     localStorage.clear();
   }
 }
