457b4a3300e60cf5ec10d69164083f26f7558e91
Mathias Rav <m@git.strova.dk>
2020-02-20 22:37:23 +0100

    SoundService: Refactor, introducing playLoop() and stopLoop()

--- src/app/services/sound.service.ts
+++ src/app/services/sound.service.ts
@@ -5,8 +5,33 @@ import { Injectable } from '@angular/core';
 })
 export class SoundService {
   private soundpath = 'assets/sounds/';
+  private loops: {[soundName: string]: HTMLAudioElement} = {};
 
   constructor() {}
+
+  public playLoop(soundName: string) {
+    if (this.loops[soundName] === undefined) {
+      const audio = this.loops[soundName] = this.play(soundName);
+      audio.loop = true;
+      // We might try to play audio at page load,
+      // but this is rejected by modern browsers (no autoplay).
+      // Work around this by starting the audio when the user clicks once.
+      const click = () => {
+        audio.play();
+        window.removeEventListener('click', click);
+      };
+      window.addEventListener('click', click);
+    } else {
+      this.loops[soundName].play();
+    }
+  }
+
+  public stopLoop(soundName: string) {
+    const audio = this.loops[soundName];
+    if (audio !== undefined) {
+      audio.pause();
+    }
+  }
 
   public play(soundName: string) {
     const audio = new Audio();
