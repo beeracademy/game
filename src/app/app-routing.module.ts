f4f6f3366f0d911b0cc78f3d4d53ce7d9bca3ac2
Asger Hautop Drewsen <asger@tyilo.com>
2023-06-08 19:23:49 +0200

    WIP angular 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,7 +28,6 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      relativeLinkResolution: "legacy",
       onSameUrlNavigation: "reload",
     }),
   ],
